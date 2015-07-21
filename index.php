0de4d309b99427f18cf5eae4d85a89f029e46594
Joe Lambert <joe@joelambert.co.uk>
2015-07-21 08:24:34 +0100

    Corrected typo

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require 'vendor/autoload.php';
 
 // Create a Primer instance
-$patternLab = require_once __DIR__.'/bootstrap/start.php';
+$primer = require_once __DIR__.'/bootstrap/start.php';
 
 // Create a router object
 $router = new AltoRouter();
@@ -11,35 +11,35 @@ $router = new AltoRouter();
 /**
  * Match the home page, show the full list of patterns
  */
-$router->map('GET', '/', function() use ($patternLab) {
-    $patternLab->showAllPatterns(!isset($_GET['minimal']));
+$router->map('GET', '/', function() use ($primer) {
+    $primer->showAllPatterns(!isset($_GET['minimal']));
 });
 
 /**
  * Match the routes for showing individual patterns
  */
-$router->map('GET', '/patterns/[**:trailing]', function( $ids ) use ($patternLab) {
+$router->map('GET', '/patterns/[**:trailing]', function( $ids ) use ($primer) {
     // Enable multiple patterns to be displayed, seperated by ':' characters
     $ids = explode(':', $ids);
 
     // Show the patterns
-    $patternLab->showPatterns($ids, !isset($_GET['minimal']));
+    $primer->showPatterns($ids, !isset($_GET['minimal']));
 });
 
 /**
  * Match the routes for showing specific templates
  */
-$router->map('GET', '/templates/[**:trailing]', function( $id ) use ($patternLab) {
+$router->map('GET', '/templates/[**:trailing]', function( $id ) use ($primer) {
     // Show the template
-    $patternLab->showTemplate($id);
+    $primer->showTemplate($id);
 });
 
 /**
  * Match the routes for retrieving the list of page templates
  */
-$router->map('GET', '/menu', function( ) use ($patternLab) {
+$router->map('GET', '/menu', function( ) use ($primer) {
     // Show the template
-    $patternLab->showMenu();
+    $primer->showMenu();
 });
 
 // Match current request url
