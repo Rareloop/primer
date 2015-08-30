c8d59f495525d8ef97927652df40983f047b59ec
Joe Lambert <joe@joelambert.co.uk>
2015-08-30 15:45:48 +0100

    Implemented Blade template engine & tests

---
+++ src/rareloop/Templating/Blade/Tests/PatternTest.php
@@ -0,0 +1,121 @@
+<?php namespace Rareloop\Templating\Balde\Tests;
+
+use Rareloop\Primer\Events\Event;
+use Symfony\Component\Finder\Finder;
+
+class PatternTest extends \PHPUnit_Framework_TestCase
+{
+    protected $primer;
+
+    /**
+     * Bootstrap the system
+     */
+    public function setup()
+    {
+        $this->primer = \Rareloop\Primer\Primer::start(__DIR__.'/primer-test', '\Rareloop\Templating\Blade\BladeTemplate');
+
+        /**
+         * Listen for when new Template engines are created
+         */
+        Event::listen('blade.new', function ($twig) {
+            // Make sure the cache is disabled otherwise we'll get unreliable test results
+            $finder = new Finder();
+            $files = $finder->files()->in(__DIR__.'/primer-test/cache');
+
+            foreach($files as $file) {
+                @unlink($file);
+            }
+        });
+    }
+
+    /**
+     * The twig template should render
+     */
+    public function testBasicRender()
+    {
+        $output = $this->primer->getPatterns(array('components/test-group/basic-pattern'), false);
+
+        $this->assertEquals($output, 'Basic pattern with no data');
+    }
+
+    /**
+     * A pattern should load data from data.json
+     */
+    public function testData()
+    {
+        $output = $this->primer->getPatterns(array('components/test-group/data-autoload'), false);
+
+        $this->assertEquals($output, 'Title autoloaded from data.json');
+    }
+
+    /**
+     * Include one pattern in another
+     */
+    public function testCustomIncludeFunction()
+    {
+        $output = $this->primer->getPatterns(array('components/test-group/include-basic'), false);
+
+        $this->assertEquals($output, 'Basic pattern with no data');
+    }
+
+    /**
+     * Test when a pattern is included that itself has default data
+     */
+    public function testCustomIncludeFunctionWithDefaultData()
+    {
+        $output = $this->primer->getPatterns(array('components/test-group/include-default-data'), false);
+
+        $this->assertEquals($output, 'Title autoloaded from data.json');
+    }
+
+    /**
+     * Test when a pattern is included that itself has default data but the parent pattern overrides the data
+     */
+    public function testCustomIncludeFunctionWithOverriddenData()
+    {
+        $output = $this->primer->getPatterns(array('components/test-group/include-override-data'), false);
+
+        $this->assertEquals($output, 'Data overridden in parent data.json');
+    }
+
+    /**
+     * Test when a pattern is included that itself has default data but the parent pattern overrides the data
+     */
+    public function testCustomIncludeFunctionWithInlineData()
+    {
+        $output = $this->primer->getPatterns(array('components/test-group/include-inline-data'), false);
+
+        $this->assertEquals($output, 'Inlined data');
+    }
+
+    /**
+     * Test that including an aliased pattern finds the template.twig file from the parent
+     */
+    public function testCustomIncludeWithAliasedPattern()
+    {
+        $output = $this->primer->getPatterns(array('components/test-group/include-aliased'), false);
+
+        $this->assertEquals($output, 'Basic pattern with no data');
+    }
+
+    /**
+     * Test that including an aliased pattern with overridden data works
+     */
+    public function testCustomIncludeWithAliasedPatternAndOverriddenData()
+    {
+        $output = $this->primer->getPatterns(array('components/test-group/include-aliased-override-data'), false);
+
+        $this->assertEquals($output, 'Title overridden from child data.json');
+    }
+
+    /**
+     * Test that including an aliased pattern with inline data works
+     */
+    public function testCustomIncludeWithAliasedPatternAndInlineData()
+    {
+        $output = $this->primer->getPatterns(array('components/test-group/include-aliased-inline-data'), false);
+
+        $this->assertEquals($output, 'Inline data');
+    }
+}
+?>
---
+++ src/rareloop/Templating/Blade/Tests/primer-test/patterns/components/test-group/basic-pattern/template.blade.php
@@ -0,0 +1,1 @@
+Basic pattern with no data
