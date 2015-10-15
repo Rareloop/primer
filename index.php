10e7ce6b88a648967a6ebbb7154d6e9e7875cc4c
Joe Lambert <joe@joelambert.co.uk>
2015-10-15 20:35:10 +0100

    PSR compliance

--- index.php
+++ index.php
@@ -11,14 +11,14 @@ $router = new AltoRouter();
 /**
  * Match the home page, show the full list of patterns
  */
-$router->map('GET', '/', function() use ($patternLab) {
+$router->map('GET', '/', function () use ($patternLab) {
     $patternLab->showAllPatterns(!isset($_GET['minimal']));
 });
 
 /**
  * Match the routes for showing individual patterns
  */
-$router->map('GET', '/patterns/[**:trailing]', function( $ids ) use ($patternLab) {
+$router->map('GET', '/patterns/[**:trailing]', function ($ids) use ($patternLab) {
     // Enable multiple patterns to be displayed, seperated by ':' characters
     $ids = explode(':', $ids);
 
@@ -29,7 +29,7 @@ $router->map('GET', '/patterns/[**:trailing]', function( $ids ) use ($patternLab
 /**
  * Match the routes for showing specific templates
  */
-$router->map('GET', '/templates/[**:trailing]', function( $id ) use ($patternLab) {
+$router->map('GET', '/templates/[**:trailing]', function ($id) use ($patternLab) {
     // Show the template
     $patternLab->showTemplate($id);
 });
@@ -37,7 +37,7 @@ $router->map('GET', '/templates/[**:trailing]', function( $id ) use ($patternLab
 /**
  * Match the routes for retrieving the list of page templates
  */
-$router->map('GET', '/menu', function( ) use ($patternLab) {
+$router->map('GET', '/menu', function () use ($patternLab) {
     // Show the template
     $patternLab->showMenu();
 });
@@ -46,8 +46,8 @@ $router->map('GET', '/menu', function( ) use ($patternLab) {
 $match = $router->match();
 
 // Call closure or throw 404 status
-if($match && is_callable($match['target'])) {
-    call_user_func_array($match['target'], $match['params']); 
+if ($match && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
 } else {
     throw new Exception('A route could not be found to match your request');
 }
